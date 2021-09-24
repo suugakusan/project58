45825240f106a094bcedd37568544fe4460c2e78
kojikoji <rubypra0804@gmail.com>
2021-09-24 19:55:59 +0900

    修正

--- lib/import_csv.rb
+++ lib/import_csv.rb
@@ -3,12 +3,19 @@ require "csv"
 class ImportCsv
   # CSVデータのパスを引数として受け取り、インポート処理を実行
   def self.import(path)
+     list = []
      CSV.foreach(path, headers: true) do |row|
-       User.create!(
-         name: row["name"],
-         age: row["age"],
-         address: row["address"]
-       )
+      list << row.to_h
      end
+     list
+  end
+
+  def self.user_data
+    # importクラスメソッドを呼び出し，ユーザーデータの配列を生成
+    list = import('db/csv_data/user_data.csv')
+
+    puts "インポート処理を開始"
+    User.create!(list)
+    puts "インポート完了!"
   end
 end
